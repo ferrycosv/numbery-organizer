9c685f0becbebec7d51d4a5185c027a41a924f06
Francisco Fernández <ferrycosv@gmail.com>
2020-03-30 10:45:52 +0200

    All at once ;)

--- object.js
+++ object.js
@@ -1,42 +1,59 @@
 const object = {
   numberyStrings: [],
   NaNyStrings: [],
-  isNumberyString: function (param) {
-
+  isNumberyString: function(param) {
+    if (typeof param !== "string") {
+      return false;
+    } else {
+      return !isNaN(param);
+    }
   },
-  addString: function (param) {
-
+  addString: function(param) {
+    if (typeof param !== "string") {
+      return false;
+    } else {
+      if (isNaN(param)) {
+        this.NaNyStrings.push(param);
+      } else {
+        this.numberyStrings.push(param);
+      }
+      return true;
+    }
   },
-  allStrings: function () {
-
+  allStrings: function() {
+    return [...this.numberyStrings, ...this.NaNyStrings];
   },
-  evenStrings: function () {
-
+  evenStrings: function() {
+    return this.numberyStrings.filter(x => Number(x) % 2 === 0);
   },
-  oddStrings: function () {
-
+  oddStrings: function() {
+    return this.numberyStrings.filter(x => Number(x) % 2 !== 0);
   },
-  negativeStrings: function () {
-
+  negativeStrings: function() {
+    return this.numberyStrings.filter(x => Number(x) < 0);
   },
-  positiveStrings: function () {
-
+  positiveStrings: function() {
+    return this.numberyStrings.filter(x => Number(x) > 0 || x === "");
   },
-  zeroStrings: function () {
-
+  zeroStrings: function() {
+    return this.numberyStrings.filter(x => Number(x) === 0 || x === "");
   },
-  numberyAsNumbers: function () {
-
+  numberyAsNumbers: function() {
+    return this.numberyStrings.map(x => Number(x));
   },
-  NaNyAsNumbers: function () {
-
+  NaNyAsNumbers: function() {
+    return this.NaNyStrings.map(x => Number(x));
   },
-  sumOfNumbery: function () {
-
+  sumOfNumbery: function() {
+    return this.numberyStrings.reduce(
+      (accumulator, currentValue) => accumulator + Number(currentValue),
+      0
+    );
   },
-  sumOfNaNy: function () {
-
-  },
+  sumOfNaNy: function() {
+    return this.NaNyStrings.reduce(
+      (accumulator, currentValue) => accumulator + Number(currentValue),
+      0
+    );
+  }
 };
-
-
